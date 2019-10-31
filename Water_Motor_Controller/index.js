2caedfe291011d9429eb505c061c77a33fe9796b
siva-piskool <siva.piskool@gmail.com>
2019-10-31 15:11:52 +0530

    New branch created

--- Water_Motor_Controller/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @format
- */
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import Demo from './Components/Demo2';
-
-AppRegistry.registerComponent(appName, () =>Demo);
