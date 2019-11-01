afa3c17da9babb91a97594f966f4cbee9864da16
siva-piskool <siva.piskool@gmail.com>
2019-11-01 11:15:42 +0530

    Contact Page Updated

--- PI-App/client/src/components/Contact.jsx
+++ PI-App/client/src/components/Contact.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
 
-export const Contact = () =>{
-    return(
-        <div>Contact</div>
-    );
-}
+export const Contact = () => {
+  return (
+    <div>
+      <div className="piheader-img">
+        <img src={require("../../public/images/about-pi.jpg")} alt="" />
+        <div class="piheader-txt">Contact</div>
+      </div>
+      <div className="piabout piMT20">
+        <div></div>
+        <div>
+          At Print Ideas, we are impulsively inclined towards transforming your
+          deliberations into designs. Yes, you read it right. Any brand is
+          undone without the branding material ensembles and that is when we
+          come into the picture. We create one of a kind identities for
+          corporate, business and individual firms by designing user interfaces,
+          digital marketing, logos, flyers, brochures, presentations and much
+          more...
+        </div>
+        <div></div>
+      </div>
+     
+    </div>
+  );
+};
