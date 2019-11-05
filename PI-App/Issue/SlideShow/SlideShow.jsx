c880887d93b1d7a82562dd7e48d89cb10b80b457
siva-piskool <siva.piskool@gmail.com>
2019-11-05 11:04:14 +0530

    Problem file added in issue folder

---
+++ PI-App/Issue/SlideShow/SlideShow.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import './show.css';
+import { Fade } from 'react-slideshow-image';
+const fadeProperties = {
+    duration: 2000,
+    transitionDuration:500,
+    infinite: true,
+    indicators: true,
+    autoplay:true,
+    onChange: (oldIndex, newIndex) => {
+      console.log(`fade transition from ${oldIndex} to ${newIndex}`);
+    }
+  }
+export default function SlideShow() {
+  const [images] = useState<Array<number>>([
+    { id: 1, pic: "img01" },
+    { id: 2, pic: "img02" },
+    { id: 3, pic: "img03" },
+    { id: 4, pic: "img04" },
+    { id: 5, pic: "img05" },
+    { id: 6, pic: "img06" }
+  ]);
+  return (
+  <div className="fade-container">
+    <Fade {...fadeProperties}>
+    {images.map(img=>(
+     <div  key={img.id} className="each-fade">
+      <div className="image-container">
+      <img src={require(`../../../public/images/pihome-${img.pic}.jpg`)} />
+      </div>
+     </div>
+    ))}
+     </Fade>
+  </div>
+   );
+}
